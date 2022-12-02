15c1717b414f867ec8be2c2ced0ea2755a58e97a
bonraton <bonraton@yandex.ru>
2022-12-02 21:23:45 +0100

    feat: basic styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,41 @@
 import './Navbar.css';
+import {
+  RiMenu3Line,
+  RiMailCloseLine,
+} from 'react-icons/ri';
+import logo from '../../assets/logo.png';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p>
+            <a href='#home'>Home</a>
+          </p>
+          <p>
+            <a href='#wgpt3'>What is GPT3</a>
+          </p>
+          <p>
+            <a href='#possipility'>Open AI</a>
+          </p>
+          <p>
+            <a href='#features'>Case Studies</a>
+          </p>
+          <p>
+            <a href='#blog'>Library</a>
+          </p>
+        </div>
+      </div>
+        <div className='gp3__navbar-sign'>
+          <p>Sign in</p>
+          <button type="button">Sign up</button>
+        </div>
+    </div>
+  );
 };
 
 export default Navbar;

16776dd0c7c83903e2ebe6918bcf8200ebba6c7c
bonraton <bonraton@yandex.ru>
2022-12-18 13:38:38 +0100

    feat: whatGPT3 container

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,7 +1,23 @@
 import './Feature.css';
 
-const Feature = () => {
-  return <div>Feature</div>;
+type Props = {
+  title: string;
+  text: string;
+};
+
+const Feature = (props: Props) => {
+  const { title, text } = props;
+  return (
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div /> 
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container__feature-text'>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
